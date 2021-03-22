4ade153794a65eb0041f3b26427032a4a9e9a2f9
jatamayo <jtamayoalejandro@gmail.com>
2021-03-21 19:00:49 -0600

    start learning react

---
+++ src/index.js
@@ -0,0 +1,25 @@
+// const element = document.createElement('h1');
+// element.innerText = 'Hello, Platzi Badges!';
+
+// const container = document.getElementById('app');
+
+// container.appendChild(element);
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+// Bootstrap
+import 'bootstrap/dist/css/bootstrap.css';
+// Styles
+import './styles.css';
+// Componentes
+import Badge from './components/Badge';
+
+const container = document.getElementById('app');
+
+// ReactDOM.render(__qué__, __dónde__);
+ReactDOM.render(<Badge 
+  firstName='Jose Alejandro' 
+  lastName='Oyamat' 
+  profession='Frontend Engineer'
+  email='jtamayo@email.com'
+  avatar='https://www.gravatar.com/avatar?d=identicon' />, container);
